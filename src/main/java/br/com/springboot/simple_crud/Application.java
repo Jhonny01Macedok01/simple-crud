0741ecd46ccc38457f3c64e452b8ea6c25fc70dd
Jhonny Macedo <jhonjhonny.rr@gmail.com>
2024-11-22 15:55:01 -0400

    implemented method listAll

--- src/main/java/br/com/springboot/simple_crud/Application.java
+++ src/main/java/br/com/springboot/simple_crud/Application.java
@@ -2,12 +2,13 @@ package br.com.springboot.simple_crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 /**
  *
  * Spring Boot application starter class
  */
-//@EntityScan(basePackages = "bbr.com.springboot.simple_crud.model")
+@EntityScan(basePackages = "br.com.springboot.simple_crud.model")
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
--- src/main/java/br/com/springboot/simple_crud/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/simple_crud/controllers/GreetingsController.java
@@ -1,10 +1,15 @@
 package br.com.springboot.simple_crud.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -44,6 +49,16 @@ public class GreetingsController {
     	
     	return "Olá mundo " + nome;
     }
+    
+    @GetMapping(value = "listatodos")
+    @ResponseBody /*Retorna os dados para o corpo da reposta*/
+    public ResponseEntity<List<Usuario>> listaUsuario() {
+    	
+    	List<Usuario> usuarios = usuarioRepository.findAll(); /*Executa consulta no DB*/
+    	
+    	return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK); /*Conulta o DB e rotorna a lista em JASON*/
+    }	
+    
 }
 
 
