e271e1669870627bd70b3d4a58d37be8e525900d
Jhonny Macedo <jhonjhonny.rr@gmail.com>
2024-11-22 13:21:44 -0400

    implemented RepositoryUsuario created method save and tested persistece data in database

--- src/main/java/br/com/springboot/simple_crud/Application.java
+++ src/main/java/br/com/springboot/simple_crud/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  * Spring Boot application starter class
  */
+//@EntityScan(basePackages = "bbr.com.springboot.simple_crud.model")
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
