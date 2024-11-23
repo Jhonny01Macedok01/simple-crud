3bfd3f32869c79a24e3afaa82a91155fb2c672ee
Jhonny Macedo <jhonjhonny.rr@gmail.com>
2024-11-23 11:47:45 -0400

    created methods: buscaruserid, atualizar, and restriction of update for id

--- src/main/java/br/com/springboot/simple_crud/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/simple_crud/controllers/GreetingsController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -71,11 +72,33 @@ public class GreetingsController {
     	 
     }
     
+    @PutMapping(value = "atualizar") /*mapeia a url*/
+    @ResponseBody /*vai fazer a descrição da Resposta*/
+    public ResponseEntity<?> atualizar(@RequestBody Usuario usuario){ /*Recebe os dados para salvar*/
+    	
+    	if (usuario.getId() == null) {	
+        	return new ResponseEntity<String>("Id do usuário não foi informado, para atualização!", HttpStatus.OK);
+
+    	}
+    	
+    	Usuario user = usuarioRepository.saveAndFlush(usuario); /*salva e já roda diretamente no BD sem precisar finalizar pra dar o commit*/
+    	return new ResponseEntity<Usuario>(user, HttpStatus.OK);
+    	 
+    }
+    
     @DeleteMapping(value = "delete") /*mapeia a url*/
     @ResponseBody /*vai fazer a descrição da Resposta*/
     public ResponseEntity<String> delete(@RequestParam Long iduser){ /*Recebe os dados para salvar*/
     	usuarioRepository.deleteById(iduser);
     	return new ResponseEntity<String>("Usuário deletado com sucesso!", HttpStatus.OK);
+    	 
+    }
+    
+    @GetMapping(value = "buscaruserid") /*mapeia a url*/
+    @ResponseBody /*vai fazer a descrição da Resposta*/
+    public ResponseEntity<Usuario> buscaruserid(@RequestParam(name = "iduser") Long iduser){ /*Recebe os dados para salvar*/
+    	Usuario  usuario = usuarioRepository.findById(iduser).get();
+    	return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
     	 
     }
     
