4f3d9999a04e1647715bfea82ce7e9babbad251a
Jhonny Macedo <jhonjhonny.rr@gmail.com>
2024-11-28 14:07:08 -0400

    created a modal screen, batle,buttons: PESQUISAR, FECHAR and VER. implemented the functions: pesquisarUser, colocarEmEdicao, salvarUsuario

--- src/main/java/br/com/springboot/simple_crud/repository/UsuarioRepository.java
+++ src/main/java/br/com/springboot/simple_crud/repository/UsuarioRepository.java
@@ -11,8 +11,7 @@ import br.com.springboot.simple_crud.model.Usuario;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 	
-	@Query(value = "select u from Usuario u where upper(trim(u.nome)) like %?1%")
-	
+	@Query(value = "select u from Usuario u where upper(trim(u.nome)) like %?1%")		
 	/*upper(trim(u.nome)) retira a restrição de inserção de espaços e ignora a exigencia de colocar letras maisculas no
 	 *  campo busca por nome sendo possivel inserir espaços e funcionar a busca*/
 	
